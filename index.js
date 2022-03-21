37a2a924461b09a08cb263707d228d36f2cb760f
cleber2099 <primermito@gmail.com>
2022-03-21 03:56:08 -0300

    Express, Ejs retornando html e varieaveis js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//Ejs como view engine/ redenrizar html
+app.set('view engine','ejs');
+
+app.get('/:nome/:lang',(req, res)=>{
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+      {  nome: "Doritos",preco: 3.12},
+      { nome: "Coca",preco: 5},
+      { nome: "Jaca",preco: 10}
+    ]
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empre: "Guia do programador",
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos:produtos
+    });
+})
+app.listen(8080,()=>{
+    console.log("App rodando")
+})
