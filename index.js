508af64febf54ba61aff3791b0a0b8ba105901fa
cleber2099 <primermito@gmail.com>
2022-03-24 22:31:43 -0300

    Bd, sequelize, mysql2

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+const connection = require('./database/database');
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+//database
+connection
+.authenticate()
+.then(()=>{
+    console.log("conexão feita com o bd")
+})
+.catch((msgErro)=>{
+    console.log(msgErro)
+})
 
 //Ejs como view engine/ redenrizar html
 app.set('view engine','ejs');
 app.use(express.static('public'));
-
-app.get('/:nome/:lang',(req, res)=>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-      {  nome: "Doritos",preco: 3.12},
-      { nome: "Coca",preco: 5},
-      { nome: "Jaca",preco: 10}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empre: "Guia do programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos:produtos
+//bodyparser
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+//rotas
+app.get('/',(req, res)=>{
+    Pergunta.findAll({raw:true}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        }); 
     });
+    
+});
+
+app.get('/perguntar',(req, res)=>{ 
+    res.render("perguntar");
+})
+app.post("/salvarpergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    //INSERINDO PERGUNTA NO BD
+    Pergunta.create({titulo: titulo,
+        descricao: descricao
+    }).then(()=>{
+        res.redirect("/");//redirecionando pag principal
+    });
+})
+
+app.get('/pergunta/:id',(req, res)=>{ 
+    var id = req.params.id;
+    Pergunta.findOne({
+        where:{id: id}
+    }).then(pergunta =>{
+        if(pergunta != undefined){
+            res.render("pergunta",{
+                pergunta: pergunta
+            });
+        }else{
+            res.redirect("/");
+        }
+    })
 })
 app.listen(8080,()=>{
     console.log("App rodando")
